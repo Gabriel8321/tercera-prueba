513d7a03e9c796b4e487855aa0d25de6cb732ede
junjun1-code <aguilarherrera.nicolas@gmail.com>
2025-11-24 14:58:12 -0300

    ODIO ERRORES DE 1 CARACTER.

--- Backend/DB/DDL.sql
+++ Backend/DB/DDL.sql
@@ -4,7 +4,7 @@ create table cliente (rut varchar(10) primary key not null, nom_c varchar(20),
 apell_c varchar(20), fono_c varchar(10), direcc varchar(20)); 
 
 create table producto (codigo_p varchar(10) primary key not null, nom_p varchar(40),
-tipo varchar(20), precio numeric(10) CHECK (precio>0), stock int CHECK (stock>=)0), imagen_url VARCHAR); 
+tipo varchar(20), precio numeric(10) CHECK (precio>0), stock int CHECK (stock>=0), imagen_url VARCHAR); 
 
 
 
