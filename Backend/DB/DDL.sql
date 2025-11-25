2713c447500bdf970ec9bcd476ef6a315bc53384
Gabriel8321 <gabrielnunezm02@gmail.com>
2025-11-24 21:34:38 -0300

    se modifico el boletas y factura para que sea un link que lleva a la factura.html, ahora se puede mostrar la tabla vista en la pagina de la factura y se elimino un espacio en vacio en la base de datos (donde se crean las tablas)

--- Backend/DB/DDL.sql
+++ Backend/DB/DDL.sql
@@ -6,8 +6,6 @@ apell_c varchar(20), fono_c varchar(10), direcc varchar(20));
 create table producto (codigo_p varchar(10) primary key not null, nom_p varchar(40),
 tipo varchar(20), precio numeric(10) CHECK (precio>0), stock int CHECK (stock>=0), imagen_url VARCHAR); 
 
-
-
 create table venta (id_v serial primary key not null, cantidad int not null CHECK (cantidad>0), fecha_compra date, 
 rut varchar(10) not null, codigo_p varchar(10) not null,
 foreign key (rut) references cliente(rut),
